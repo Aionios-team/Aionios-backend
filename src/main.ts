a52fd822cc82f2cddb6d2109bd64fa4683c1b4ad
Sergio Ernesto Rosas Ducoing <rosasducoing@gmail.com>
2026-05-27 13:10:28 -0600

    add CORS config for browser requests

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { rawBody: true });
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3001',
+      process.env.FRONTEND_URL,
+    ].filter(Boolean) as string[],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
